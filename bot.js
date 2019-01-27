482efbc62f5cd5fa9224365e135e0706eb115305
Moha-xD <47069367+Moha-xD@users.noreply.github.com>
2019-01-27 02:07:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,66 +1,102 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord  = require('discord.js');
+const hero     = new Discord.Client();
+const prefix   = "#";
+const category = "530162023749779462";
+const devs     = ["475396751549792277", "403512493529497601"];
+let mtickets   = true;
+let tchannels  = [];
+let current    = 0;
 
-client.on('ready', () => {
-    console.log('I am ready!');
+hero.login('NTI3OTY4Mzg0MTY0NDI5ODI0.Dw7B2A.kPwmsJGmbs5ItSeDcKSkjJ_XR58');
+
+hero.on('ready',async () => console.log(`   - " ${hero.user.username} " , Tickety is ready to work.`));
+hero.on('message',async message => {
+    const emojis   = { yes: `${hero.guilds.find(r => r.id === '530036295225835520').emojis.find(e => e.name === 'Yes')}`, wrong: `${hero.guilds.find(r => r.id === '530036295225835520').emojis.find(e => e.name === 'Wrong')}` };
+    if(message.author.bot || message.channel.type === 'dm') return;
+    let args = message.content.split(" ");
+    let author = message.author.id;
+    if(args[0].toLowerCase() === `${prefix}help`) {
+            let embed = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)
+            .setThumbnail(message.author.avatarURL)
+            .setColor("#36393e")
+            .addField(`❯ لعمل تكت, \`${prefix}new\``, `» Syntax: \`${prefix}new [Reason]\`\n» Description: **لعمل روم فقط يظهر لك ولأدارة السيرفر.**`)
+            .addField(`❯ قائمة الأوامر, \`${prefix}help\``, `» Syntax: \`${prefix}help\`\n» Description: **يظهر لك جميع اوامر البوت.**`)
+            .addField(`❯ لإيقاف الأعضاء من عمل تكتات, \`${prefix}mtickets\``, `» Syntax: \`${prefix}mtickets [Disable/Enable]\`\n» Description: **لجعل جميع اعضاء السيرفر غير قادرون على عمل تكت.**`)
+			.addField(`❯ لأقفال جميع التكتات المفتوحة, \`${prefix}deletetickets\``, `» Syntax: \`${prefix}deletetickets\`\n» Description: **لمسح جميع رومات التكتات المفتوحة في السيرفر**`)
+            .addField(`❯ لقفل التكت المفتوح, \`${prefix}close\``, `» Syntax: \`${prefix}close\`\n» Description: **لأقفال تكت.**\n\n للمزيد من المعلومات تواصل مع احد ادارة سيرفر رويال جيمز.`)
+            await message.channel.send(`${emojis.yes}, **هذه قائمة بجميع اوامر البووت.**`);
+            await message.channel.send(embed);
+    } else if(args[0].toLowerCase() === `${prefix}new`) {
+        if(mtickets === false) return message.channel.send(`${emojis.wrong}, **تم ايقاف هذه الخاصية من قبل احد ادارة السيرفر**`);
+        if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send(`${emojis.wrong}, **البوت لا يملك صلاحيات لصنع الروم**`);
+		console.log(current);
+		let openReason = "";
+		current++;
+    	message.guild.createChannel(`rgticket-${current}`, 'text').then(c => {
+		tchannels.push(c.id);
+		c.setParent(category);
+		message.channel.send(`${emojis.yes}, **تم عمل التكت.**`);
+		c.overwritePermissions(message.guild.id, {
+			READ_MESSAGES: false,
+			SEND_MESSAGES: false
+		});
+		c.overwritePermissions(message.author.id, {
+			READ_MESSAGES: true,
+			SEND_MESSAGES: true
+		});
+		
+		if(args[1]) openReason = `\nسبب فتح التكت , " **${args.slice(1).join(" ")}** "`;
+		let embed = new Discord.RichEmbed()
+		.setAuthor(message.author.username, message.author.avatarURL)
+		.setColor("#36393e")
+		.setDescription(`**انتظر قليلا الى حين رد الادارة عليك**${openReason}`);
+		c.send(`${message.author}`);
+		c.send(embed);
+	});
+    } else if(args[0].toLowerCase() === `${prefix}mtickets`) {
+        if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`${emojis.wrong}, **أنت لست من ادارة السيرفر لتنفيذ هذا الأمر.**`);
+		if(args[1] && args[1].toLowerCase() === "enable") {
+			mtickets = true;
+			message.channel.send(`${emojis.yes}, **تم تفعيل التكتات , الاَن يمكن لأعضاء السيرفر استخدام امر انشاء التكت**`);
+		} else if(args[1] && args[1].toLowerCase() === "disable") {
+			mtickets = false;
+			message.channel.send(`${emojis.yes}, **تم اغلاق نظام التكتات , الاَن لا يمكن لأي عضو استخدام هذا الأمر**`);
+		} else if(!args[1]) {
+			if(mtickets === true) {
+			mtickets = false;
+			message.channel.send(`${emojis.yes}, **تم اغلاق نظام التكتات , الاَن لا يمكن لأي عضو استخدام هذا الأمر**`);
+			} else if(mtickets === false) {
+			mtickets = true;
+			message.channel.send(`${emojis.yes}, **تم تفعيل التكتات , الاَن يمكن لأعضاء السيرفر استخدام امر انشاء التكت**`);
+			}
+		}
+    } else if(args[0].toLowerCase() === `${prefix}close`) {
+		if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`${emojis.wrong}, **أنت لست من ادارة السيرفر لتنفيذ هذا الأمر.**`);
+		if(!message.channel.name.startsWith('rgticket-') && !tchannels.includes(message.channel.id)) return message.channel.send(`${emojis.wrong}, **هذا الروم ليس من رومات التكت.**`);
+		
+		message.channel.send(`${emojis.yes}, **سيتم اغلاق الروم في 3 ثواني من الاَن.**`);
+		tchannels.splice( tchannels.indexOf(message.channel.id), 1 );
+		setTimeout(() => message.channel.delete(), 3000);
+	} else if(args[0].toLowerCase() === `${prefix}restart`) {
+		if(!devs.includes(message.author.id)) return message.channel.send(`${emojis.wrong}, **أنت لست من ادارة السيرفر لأستخدام هذا الأمر.**`);
+		message.channel.send(`${emojis.yes}, **جارى اعادة تشغيل البوت.**`);
+		hero.destroy();
+		hero.login(process.env.ROYALE_TOKEN);
+	} else if(args[0].toLowerCase() === `${prefix}deletetickets`) {
+		let iq = 0;
+		for(let q = 0; q < tchannels.length; q++) {
+			let c = message.guild.channels.get(tchannels[q]);
+			if(c) {
+				c.delete();
+				tchannels.splice( tchannels[q], 1 );
+				iq++;
+			}
+			if(q === tchannels.length - 1 || q === tchannels.lengh + 1) {
+				message.channel.send(`${emojis.yes}, **تم مسح \`${iq}\` من التكتات.**`);
+			}
+		}
+	}
 });
 
-var config = {
-  events: [
-    {type: "CHANNEL_CREATE", logType: "CHANNEL_CREATE", limit: 4 , delay: 5000},
-    {type: "CHANNEL_DELETE", logType: "CHANNEL_DELETE", limit: 4, delay: 5000},
-    {type: "GUILD_MEMBER_REMOVE", logType: "MEMBER_KICK", limit: 4, delay: 5000},
-    {type: "GUILD_BAN_ADD", logType: "MEMBER_BAN_ADD", limit: 4, delay: 5000},
-    {type: "GUILD_ROLE_CREATE", logType: "ROLE_CREATE", limit: 5, delay: 5000},
-    {type: "GUILD_ROLE_DELETE", logType: "ROLE_DELETE", limit: 4, delay: 5000},
-  ]
-}
-client.on("error", (e) => console.error(e));
-client.on("raw", (packet)=> {
-  let {t, d} = packet, type = t, {guild_id} = data = d || {};
-  if (type === "READY") {
-    client.startedTimestamp = new Date().getTime();
-    client.captures = [];
-  }
-  let event = config.events.find(anEvent => anEvent.type === type);
-  if (!event) return;
-  let guild = client.guilds.get(guild_id);
-  if (!guild) return;
-  guild.fetchAuditLogs({limit : 1, type: event.logType})
-    .then(eventAudit => {
-      let eventLog = eventAudit.entries.first();
-      if (!eventLog) return;
-      let executor = eventLog.executor;
-      guild.fetchAuditLogs({type: event.logType, user: executor})
-        .then((userAudit, index) => {
-          let uses = 0;
-          userAudit.entries.map(entry => {
-            if (entry.createdTimestamp > client.startedTimestamp && !client.captures.includes(index)) uses += 1;
-          });
-          setTimeout(() => {
-            client.captures[index] = index
-          }, event.delay || 2000)
-          if (uses >= event.limit) {
-            client.emit("reachLimit", {
-              user: userAudit.entries.first().executor,
-              member: guild.members.get(executor.id),
-              guild: guild,
-              type: event.type,
-            })
-          }
-        }).catch(console.error)
-    }).catch(console.error)
-});
-client.on("reachLimit", (limit)=> {
-  let log = limit.guild.channels.find( channel => channel.name === "log");
-  log.send(limit.user.username+"\ntried to hack (!)");
-  limit.guild.owner.send(limit.user.username+"\ntried to hack (!)")
-  limit.member.roles.map(role => {
-    limit.member.removeRole(role.id)
-    .catch(log.send)
-  });
-});
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-
